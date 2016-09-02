b4593b9a6b14ad7989ea2de833bf6928e0a8b251
Waldomiro Seabra <wjgs@icomp.ufam.edu.br>
2016-09-01 21:48:32 -0400

    redirecionamento e pipe

--- shell.c
+++ shell.c
@@ -17,7 +17,7 @@ void getAllCommands(){
 		i++;
 		atual = (char*) malloc(sizeof(char)*50);
 
-	//	if(i == 10) break;
+		//	if(i == 10) break;
 	}
 	all_commands[i] = NULL;
 	printf("%p\n",all_commands[i]);
@@ -205,6 +205,61 @@ int mysh_launch(char **args){
 	return 1;
 }
 
+int redirecionamento(char **args, int pos){
+	int fd;
+	int pid;
+	if((pid=fork())==-1){
+		printf("Child process could not be created\n");
+		return 0;
+	}
+	if(pid==0){
+		if (strcmp(args[pos],">") == 0){
+			args[pos] = NULL;
+			fd = open(args[pos+1], O_CREAT | O_TRUNC | O_WRONLY, 0600); 
+			dup2(fd, STDOUT_FILENO); 
+			close(fd);
+		}else {
+			args[pos] = NULL;
+			fd = open(args[pos+1], O_CREAT | O_TRUNC | O_WRONLY, 0600);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+
+		char cwd[1024];
+		setenv("parent",getcwd(cwd, 1024),1);
+		if (execvp(args[0],args)==-1){
+			printf("err");
+			kill(getpid(),SIGTERM);
+		} 
+	}
+	waitpid(pid,NULL,0);
+	return 1;
+}
+
+int mysh_pipe(char **args, int pos){
+	args[pos] = NULL;
+	int pfds[2];
+	int pid, pid2;
+	pipe(pfds);
+	if (pid = fork() == 0) {
+		close(1);       
+		dup(pfds[1]);   
+		close(pfds[0]); 
+		execvp(args[0], args);
+	}
+	if (pid2 = fork() == 0){
+		close(0);       
+		dup(pfds[0]);   
+		close(pfds[1]); 
+		execvp(args[pos+1],&args[pos+1]);
+	}
+	if(pid != 0 && pid2 != 0){
+		waitpid(pid,NULL,0);
+		waitpid(pid2,NULL,0);
+	}
+	return 1;
+}
+
 int mysh_exec(char **args){
 	int i;
 
@@ -214,6 +269,18 @@ int mysh_exec(char **args){
 		if(strcmp(args[0],comandos[i]) == 0)
 			return (*comandos_func[i])(args);
 	}
+
+	i = 0;
+	while (args[i]) {
+		if (strcmp(args[i],">") == 0 || strcmp(args[i],"<") == 0) {
+			return redirecionamento(args,i);
+		}
+		if (strcmp(args[i],"|")== 0) {
+			return mysh_pipe(args,i);
+		}
+		i++;
+	}
+
 	return mysh_launch(args);
 }
 
